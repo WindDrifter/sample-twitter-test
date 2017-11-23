e1103cb6e19e39cf9005a6a02fb1c1791b91bbaa
WindDrifter <ryanjcua@msn.com>
2017-11-22 18:58:34 -0800

    remove sqllite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20171122191851) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "followings", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "following"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_followings_on_user_id"
+    t.index ["user_id"], name: "index_followings_on_user_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20171122191851) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +41,6 @@ ActiveRecord::Schema.define(version: 20171122191851) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "followings", "users"
+  add_foreign_key "posts", "users"
 end
